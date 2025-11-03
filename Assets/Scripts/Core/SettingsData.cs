372caf3307ed408b5b2445fb2944e50b69b68431
AnttiN <fanno004@edu.xamk.fi>
2025-11-03 13:13:13 +0200

    Game Loading basic functions working

---
+++ Assets/Scripts/Core/SettingsData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class SettingsData : SingletonNonMono<SettingsData>
+{
+    //Just for example purposes
+    public float musicVolume = 1.0f;
+    public float sfxVolume = 1.0f;
+}
