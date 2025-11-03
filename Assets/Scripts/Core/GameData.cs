015cdd6097275a44b47a1ff492a0795f1c14a87b
AnttiN <fanno004@edu.xamk.fi>
2025-11-03 21:23:42 +0200

    Auto stash before merge of "Antti" and "origin/Antti"

--- Assets/Scripts/Core/GameData.cs
+++ Assets/Scripts/Core/GameData.cs
@@ -5,7 +5,7 @@ public class GameData : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("GameData initialized.");
     }
 
     // Update is called once per frame
